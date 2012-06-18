e4991c9fcd3b6ce60a6c8530754e3d1e29407933
James Casbon <james.casbon@popgentech.com>
2012-06-18 14:12:07 +0100

    add hasinterval

--- ucsc/__init__.py
+++ ucsc/__init__.py
@@ -1,10 +1,21 @@
 VERSION = 0.2
 
 from db import create_session, tables
-from config import use_genome
+import config
 
 
 def use(name):
     session = create_session(name)
-    genome = use_genome(name)
+    genome = config.use_genome(name)
     return session, genome
+
+
+class HasInterval(object):
+    """ Mix in for classes with the standard interval attributes """
+
+    @property
+    def interval(self):
+        return config.genome.interval(self.start, self.end, chrom=self.chrom,
+            strand=self.strand, value=self)
+
+
